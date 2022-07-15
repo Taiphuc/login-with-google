718536df547968e035b69872a11b31f7c88023b0
Taiphuc <el.taiphuc123@gmail.com>
2022-07-15 09:54:12 +0700

    test open sms

--- src/Login.js
+++ src/Login.js
@@ -23,9 +23,10 @@ function Login() {
   return (
     <div className="login-buttons">
         <button className="login-provider-button" onClick={signInWithGoogle}>
-        <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon"/>
-        <span> Continue with Google</span>
+          <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon"/>
+          <span> Continue with Google</span>
        </button>
+       <a href="sms:9285&body=Y ED1">Test open sms</a>
       </div>
   );
 }
